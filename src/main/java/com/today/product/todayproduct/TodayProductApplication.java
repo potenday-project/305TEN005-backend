49cd3a23d6e9f0d3b2ec7f4a496255b484163a7b
vcho1958 <gusckd57@gmail.com>
2023-05-20 00:44:28 +0900

    feat(project): init project

---
+++ src/main/java/com/today/product/todayproduct/TodayProductApplication.java
@@ -0,0 +1,13 @@
+package com.today.product.todayproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayProductApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TodayProductApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/today/product/todayproduct/TodayProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.today.product.todayproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodayProductApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
